0fd363107e3747990ca5c37c075bb6a5f07bc0bb
jhtl08 <jason.lorenzo@obf.ateneo.edu>
2022-09-27 00:10:37 +0800

    idk why this doesn't work

--- sortmain.cpp
+++ sortmain.cpp
@@ -4,8 +4,10 @@
 //September 26, 2022
 
 #include <iostream>
+#include <chrono>
 #include "algosource.h"
 
+using namespace std::chrono;
 using namespace std;
 
 int main()
@@ -13,5 +15,38 @@ int main()
   //calls the function to generate unsorted array not to be modified
   int* unsortedOriginal= generateArray();
 
+  int counter=0;
+  int run_count=3;
+
+  int *bsortCounts, *isortCounts, *ssortCounts=new int(10);
+
+  for(int i=0; i<run_count;i++)
+  {
+    //initializes a time point in which an algorithm should stop
+    std::chrono::time_point<std::chrono::system_clock> end;
+    //should stop after 5 seconds
+    std::chrono::seconds duration(5);
+    end = std::chrono::system_clock::now() + duration;
+  
+    //run bubble sort while it's not yet time point end
+    cout<<"Test";
+    while(true)
+    {
+      cout<<"test 1";
+      bubblesort(unsortedOriginal);
+      counter++;
+      if (std::chrono::system_clock::now() > end)
+      {
+        break;
+      }
+    }
+    cout<<"done";
+    bsortCounts[run_count]=counter;
+  }
+
+  for(int i=0; i<run_count;i++)
+  {
+    cout<<bsortCounts[i]<<" ";
+  }
   return 0;
 }
