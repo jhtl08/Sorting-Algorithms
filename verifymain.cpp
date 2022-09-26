d03252a472b75585c8fa63fe81aec70bb20ba3f1
jhtl08 <jason.lorenzo@obf.ateneo.edu>
2022-09-26 23:27:01 +0800

    separated verification to main

---
+++ verifymain.cpp
@@ -0,0 +1,79 @@
+//verifymain.cpp
+//Kyle Coloma, Jason Lorenzo, Paolo Ong
+//ENGG 24-J
+//September 26, 2022
+
+#include <iostream>
+#include "algosource.h"
+
+using namespace std;
+
+int main()
+{
+  //calls the function to generate unsorted array not to be modified
+  int* unsortedOriginal= generateArray();
+
+
+  int*bubblesorted=bubblesort(unsortedOriginal);
+  cout<<"For Bubble Sort: ";
+//output of sorted array from bubble sort entered to verify result
+  if (verifySort(bubblesorted))
+  {
+    cout<<"The array has been verified as sorted."<<endl;
+  }
+  else
+  {
+    cout<<"The array has been verified as not sorted."<<endl;
+  }
+
+//to check manually if it's sorted
+  for (int i=0;i<10;i++)
+    {
+      cout<<bubblesorted[i]<<" ";
+    }
+  cout<<"\n\n";  
+
+
+  int*insertionsorted=insertionsort(unsortedOriginal);
+  cout << "For Insertion Sort: ";
+
+  if (verifySort(insertionsorted))
+  {
+      cout << "The array has been verified as sorted." << endl;
+  }
+  else
+  {
+      cout << "The array has been verified as not sorted." << endl;
+  }
+
+//to check manually if it's sorted
+  for (int i=0;i<10;i++)
+    {
+      cout<<insertionsorted[i]<<" ";
+    }
+    cout<<"\n\n";
+
+
+  cout<<"For Selection Sort: ";
+
+  int*selectionsorted=selectionsort(unsortedOriginal);
+
+  if (verifySort(selectionsorted))
+  {
+    cout<<"The array has been verified as sorted."<<endl;
+  }
+  else
+  {
+    cout<<"The array has been verified as not sorted."<<endl;
+  }
+
+//to check manually if it's sorted
+  for (int i=0;i<10;i++)
+    {
+      cout<<selectionsorted[i]<<" ";
+    }
+    cout<<"\n\n";
+
+  //system("pause");
+  return 0;
+}
